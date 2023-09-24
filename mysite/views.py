d0b8034b7480346782c9494f6cc506813637eccf
unknown <samarendrakuila@gmail.com>
2023-09-24 21:37:39 +0530

    MyWeb

---
+++ mysite/views.py
@@ -0,0 +1,11 @@
+from urllib import request
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+
+def form(request):
+    return render(request,'form.html')
+
+
